39f8265e3b217ff851337912516212ef4f3fb8c0
Eric Yim <TK@Erics-MacBook-Air.local>
2020-06-10 20:50:29 -0400

    update

--- game_stats.py
+++ game_stats.py
@@ -6,56 +6,42 @@ class Character():
     def hit(self, char):
         char.health -= self.attack
 
-    def move(self, dir):
-        if dir == "left":
-            self.position["x"] -= self.speed
-        elif dir == "right":
-            self.position["x"] += self.speed
-        elif dir == "up":
-            self.position["y"] -= self.speed
-        elif dir == "down":
-            self.position["y"] += self.speed
-
 
 class Knight(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 200
-        self.speed = 10
-        self.defense = 50
-        self.attack = 20 
+        self.health = 150
+        self.defense = 20
+        self.attack = 25
 
 
 class Warrior(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 150
-        self.speed = 14
-        self.defense = 20
+        self.health = 100
+        self.defense = 5
         self.attack = 40
 
     def potion(self, char):
-        char.health += 50
-        return "50"
+        char.health += 25
+        return "25"
 
 
 class Healer(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 100
-        self.speed = 12
-        self.defense = 10
+        self.health = 75
+        self.defense = 0
         self.attack = 20
 
     def heal(self, char):
-        char.health += 200
+        char.health += 75
 
 
 class Enemy(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 130
-        self.speed = 10
+        self.health = 150
         self.defense = 20
         self.attack = 30
 
