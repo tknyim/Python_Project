dbca1e2969fd236296d9810eb8a4ca2e105f1d27
Eric Yim <TK@Erics-MacBook-Air.local>
2020-06-11 16:47:10 -0400

    4:00 PM

--- board.py
+++ board.py
@@ -9,7 +9,12 @@ def gen_board(width,length):
         board.append(row)
     return board
 
-
+def starting_position(board):
+    main_char = {"x":random.randint(0,5),"y":random.randint(0,5)}
+    distance = 0
+    while(distance <= 3):
+        boss = {"x":random.randint(0,5),"y":random.randint(0,5)}
+        distance = calc_distance(main_char,boss)
 
 def calc_distance(main_char, boss):
     distance = (boss.x-main_char.x) + (boss.y-main_char.y)
--- game_stats.py
+++ game_stats.py
@@ -10,32 +10,37 @@ class Character():
 class Knight(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 150
-        self.defense = 20
+        self.health = 200
+        self.defense = 5
         self.attack = 25
+
+    def potion(self, char):
+        char.health += 25
+        return "25"
 
 
 class Warrior(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 100
-        self.defense = 10
+        self.health = 120
+        self.defense = 5
         self.attack = 40
 
     def potion(self, char):
-        char.health += 35
-        return "35"
+        char.health += 30
+        return "30"
 
 
 class Healer(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
-        self.health = 75
+        self.health = 100
         self.defense = 5
-        self.attack = 20
+        self.attack = 15
 
     def heal(self, char):
-        char.health += 75
+        char.health += 40
+        return "40"
 
 
 class Mob(Character):
@@ -43,7 +48,7 @@ class Mob(Character):
         super().__init__(name, position)
         self.health = 100
         self.defense = 5
-        self.attack = 5
+        self.attack = 8
 
 class Enemy(Character):
     def __init__(self, name, position):
@@ -57,12 +62,12 @@ class Semi_Boss(Character):
         super().__init__(name, position)
         self.health = 200
         self.defense = 15
-        self.attack = 10
+        self.attack = 12
 
 class Boss(Character):
     def __init__(self, name, position):
         super().__init__(name, position)
         self.health = 300
         self.defense = 20
-        self.attack = 10
+        self.attack = 15
 
